2c9774fe32ae82a495281e0f4d6c3f3ed9f0a850
alaasassii <alaasassijs@gmail.com>
2023-09-16 19:02:28 +0100

    add 3 new pages

---
+++ src/routes/showData/showData.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import PageUnavailable from '../../components/common/pageUnavailable'
+
+const showData = () => {
+    return (
+        <PageUnavailable
+            title='Coming Soon'
+            titleExplanation='Page Under Maintenance'
+            paragraph="We're currently working on this page to make it even better. Please check back later for updates!"
+        />
+    )
+}
+
+export default showData
