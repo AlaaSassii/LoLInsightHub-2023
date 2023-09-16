0ae7328111473cbfe9d837d73a9dccb851a244ec
alaasassii <alaasassijs@gmail.com>
2023-09-16 21:07:05 +0100

    add style

--- src/components/specefic/home/welcomeSection/welcomeSection.tsx
+++ src/components/specefic/home/welcomeSection/welcomeSection.tsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import './welcomeSection.scss'
-
+import { logoName } from '../../../../assets/logoName';
+import jinxImage from '../../../../assets/images/transparentChampions/jinx.png'
 const WelcomeSection = () => {
     return (
         <div className='welcome__section__home'>
-            <div className='big__text'></div>
-            <div className='big__text'><span></span>,</div>
-            <p></p>
+            <div className='welcome__section__text'>
+                <div className='big__text'>Welcome to</div>
+                <div className='big__text'><span className='logo__name'>{logoName}</span>,</div>
+                <p>where we analyze League of Legends like it's our job... because it kinda is! Join us on this wild ride through the Rift, where we promise the only toxicity you'll find is in our jokes, not our community. Let's dive in and have some fun!</p>
+            </div>
+            <img src={jinxImage} alt="welcome image" />
         </div>
     )
 }
