2c25e40e1605e63852a12ae46127c61f4b70344c
alaasassii <alaasassijs@gmail.com>
2023-09-16 18:09:36 +0100

    update styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
-import Toggle from './components/common/toggle/toggle'
 import { Route, Routes } from 'react-router-dom'
 import Navbar from './components/common/navbar'
 import PageNotFound from './routes/pageNotFound'
+import { navLinks } from './helpers/navlinks'
 function App() {
   return (
     <>
-      <Navbar links={[{ name: 'home', path: '/' }, { name: 'Champion & Match Hub', path: '/search' }, { name: 'games', path: '/games' }]} />
-
+      <Navbar links={navLinks} />
       <Routes>
         <Route path='/' element={<h1>Home</h1>} />
         <Route path='/search' element={<h1>search</h1>} />
@@ -15,6 +14,48 @@ function App() {
         <Route path='*' element={<PageNotFound />} />
 
       </Routes>
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <input type="text" placeholder='input test' />
+      <input type="text" placeholder='input test' />
+      <input type="text" placeholder='input test' />
+      <input type="text" placeholder='input test' />
+      <input type="text" placeholder='input test' />
+      <input type="text" placeholder='input test' />
+      <input type="text" placeholder='input test' />
+      <input type="text" placeholder='input test' />
+      <input type="text" placeholder='input test' />
+      <input type="text" placeholder='input test' />
+      <input type="text" placeholder='input test' />
+      <input type="text" placeholder='input test' />
+      <input type="text" placeholder='input test' />
+      <input type="text" placeholder='input test' />
+      <input type="text" placeholder='input test' />
+      <input type="text" placeholder='input test' />
+      <input type="text" placeholder='input test' />
+      <input type="text" placeholder='input test' />
+      <input type="text" placeholder='input test' />
+      <input type="text" placeholder='input test' />
+      <input type="text" placeholder='input test' />
+      <textarea name="" id="" cols="30" rows="10"></textarea>
     </>
   )
 }
---
+++ src/helpers/navlinks.ts
@@ -0,0 +1,9 @@
+import { links } from "../types/navbarType";
+
+export const navLinks: links = [
+    { name: 'home', path: '/' },
+    { name: 'Champion & Match Hub', path: '/search' },
+    { name: 'Games Hub', path: '/games' },
+    { name: 'contact us', path: '/contact' },
+
+]
