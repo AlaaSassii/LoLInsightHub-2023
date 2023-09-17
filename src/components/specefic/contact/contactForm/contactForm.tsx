9509d04be78dca03f790969398dd27afe4132d0d
alaasassii <alaasassijs@gmail.com>
2023-09-17 14:24:25 +0100

    add new thnigns

--- src/components/specefic/contact/contactForm/contactForm.tsx
+++ src/components/specefic/contact/contactForm/contactForm.tsx
@@ -1,4 +1,4 @@
-import { useRef } from 'react'
+import { FormEvent, useRef } from 'react'
 import './contactForm.scss';
 
 const ContactForm = () => {
@@ -6,15 +6,29 @@ const ContactForm = () => {
     const inputEmailRef = useRef<HTMLInputElement | null>(null);
     const inputSubjetRef = useRef<HTMLInputElement | null>(null);
     const inputMessageRef = useRef<HTMLTextAreaElement | null>(null);
+    const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        const name = inputNameRef.current?.value;
+        const email = inputEmailRef.current?.value;
+        const subject = inputSubjetRef.current?.value;
+        const message = inputMessageRef.current?.value;
+        if (!name || !email || !subject || !message) {
+            alert('Please fill all the fields');
+            return;
+        }
+        // send the data to the server
+        alert(`Name: ${name}\nEmail: ${email}\nSubject: ${subject}\nMessage: ${message}`);
+    }
     return (
-        <div className='contact__form'>
+        <form className='contact__form' onSubmit={handleSubmit}>
             <div className="user_data">
                 <input type="text" placeholder='name' ref={inputNameRef} />
                 <input type="email" placeholder='email' ref={inputEmailRef} />
             </div>
             <input type="text" placeholder='subject' ref={inputSubjetRef} />
             <textarea placeholder='message' ref={inputMessageRef} rows={10}></textarea>
-        </div>
+            <button type='submit'>Submit</button>
+        </form>
     )
 }
 
