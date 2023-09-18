e52d9f7bd49b0ef47835dca12e80219a51ab9d01
alaasassii <alaasassijs@gmail.com>
2023-09-18 09:11:46 +0100

    add new things

--- src/helpers/search/index.ts
+++ src/helpers/search/index.ts
@@ -8,13 +8,13 @@ export const cards: cardServiceSection[] = [
         title: 'Champion Insights',
         image: ChampionsInsightImage,
         text: "Champion Insights: Dive deep into the strengths, weaknesses, and secrets of every League of Legends champion. ",
-        navigateLink: links.HOME
+        navigateLink: links.CHAMPIONS
     },
     {
         title: 'Match History Insights',
         image: MatchHistoryInsightsImage,
         text: 'Where your victories are celebrated like championship wins, and your defeats are... just opportunities for epic comebacks. ',
-        navigateLink: links.HOME
+        navigateLink: links.MATCHHISTORY
     },
 
 ]
