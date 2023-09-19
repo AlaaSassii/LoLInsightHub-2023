763766534a199ca27173455cffa504574b45f270
alaasassii <alaasassijs@gmail.com>
2023-09-19 14:54:25 +0100

    add new things

--- src/components/specefic/searchChampions/championsContainer/championsContainer.tsx
+++ src/components/specefic/searchChampions/championsContainer/championsContainer.tsx
@@ -12,16 +12,20 @@ const ChampionsContainer: FC<championContainerType> = ({ data, championName }) =
 
     let navigate = useNavigate();
     const { data: champions } = data;
+    const championsData = Object.keys(champions).filter(name => name.toLowerCase().includes(championName.toLowerCase()));
     return (
         <div className='champions__container'>
-            {Object.keys(champions)
-                .filter(name => name.toLowerCase().includes(championName.toLowerCase()))
-                .map((name, index) => {
-                    return <div key={`champion__${index}`}>
-                        <img src={championSquareImage(name)} alt={`${name} image`} />
-                        <b>{name}</b>
-                    </div>
-                })}
+            {championsData.length === 0 ?
+                <div className='champions__container__empty'>
+                    <h1>No Champions Found</h1>
+                </div> :
+                championsData
+                    .map((name, index) => {
+                        return <div key={`champion__${index}`}>
+                            <img src={championSquareImage(name)} alt={`${name} image`} />
+                            <b>{name}</b>
+                        </div>
+                    })}
         </div>
     )
 }
