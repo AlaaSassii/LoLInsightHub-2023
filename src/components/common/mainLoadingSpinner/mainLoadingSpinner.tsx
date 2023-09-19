135a345f2fc6c9f521b3063d096ac9d56b0666ca
alaasassii <alaasassijs@gmail.com>
2023-09-19 12:06:51 +0100

    add new loading spinner

---
+++ src/components/common/mainLoadingSpinner/mainLoadingSpinner.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import './mainLoadingSpinner.scss';
+const MainLoadingSpinner = () => {
+    return (
+        <div>mainLoadingSpinner</div>
+    )
+}
+
+export default MainLoadingSpinner
