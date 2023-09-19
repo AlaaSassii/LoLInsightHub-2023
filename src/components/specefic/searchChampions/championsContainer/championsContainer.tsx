f57d8636888052a75880f80d89c6b3d0993b6eb3
alaasassii <alaasassijs@gmail.com>
2023-09-19 14:19:59 +0100

    add new things

--- src/components/specefic/searchChampions/championsContainer/championsContainer.tsx
+++ src/components/specefic/searchChampions/championsContainer/championsContainer.tsx
@@ -1,19 +1,25 @@
 import { FC } from 'react'
+import { useNavigate } from 'react-router-dom'
 import { championsDataType } from '../../../../types/championsDataType'
+import { championSquareImage } from '../../../../helpers/championSquareImage'
 
 export type championContainerType = {
     data: championsDataType
 }
 
 const ChampionsContainer: FC<championContainerType> = ({ data }) => {
+    let navigate = useNavigate();
     const { data: champions } = data;
     return (
-        <>
+        <div className='champions__container'>
             {Object.keys(champions)
                 .map((name, index) => {
-                    return <div key={index}>{name}</div>
+                    return <div key={`champion__${index}`}>
+                        <img src={championSquareImage(name)} alt={`${name} image`} />
+                        <b>{name}</b>
+                    </div>
                 })}
-        </>
+        </div>
     )
 }
 export default ChampionsContainer
