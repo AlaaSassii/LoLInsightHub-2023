edcc8c58aa104cdfcfaae29090266c3482cc2d50
alaasassii <alaasassijs@gmail.com>
2023-09-21 18:35:40 +0100

    add new things

--- src/components/specefic/singleChampionInfo/championDef/championDef.tsx
+++ src/components/specefic/singleChampionInfo/championDef/championDef.tsx
@@ -1,50 +1,14 @@
-import React from 'react'
+import React, { FC } from 'react'
 import CardContainer from '../../../common/cardContainer'
-type ChampionDef = {
-    image: {
-        full: string;
-        sprite: string;
-        group: string;
-        x: number;
-        y: number;
-        w: number;
-        h: number;
-    },
-    title: string,
-    spells: {
-        id: string;
-        name: string;
-        description: string;
-        tooltip: string;
-        leveltip: {
-            label: string[];
-            effect: string[];
-        };
-        maxrank: number;
-        cooldown: number[];
-        cooldownBurn: string;
-        cost: number[];
-        costBurn: string;
-        resource: string;
-        range: number[];
-        rangeBurn: string;
-    }[],
-    passive: {
-        name: string;
-        description: string;
-        image: {
-            full: string;
-            sprite: string;
-            group: string;
-            x: number;
-            y: number;
-            w: number;
-            h: number;
-        };
-    }
+import { image, passive, skins, spells } from '../../../../types/championDataType'
+type ChampionDefProps = {
+    blurb: string,
+    image: image,
+    spells: spells,
+    passive: passive
 
 }
-const ChampionDef = () => {
+const ChampionDef: FC<ChampionDefProps> = () => {
     return (
         <CardContainer loading={false}>championDef</CardContainer>
     )
--- src/types/championDataType.ts
+++ src/types/championDataType.ts
@@ -1,3 +1,77 @@
+export type image = {
+    full: string;
+    sprite: string;
+    group: string;
+    x: number;
+    y: number;
+    w: number;
+    h: number;
+}
+export type skins = {
+    id: string;
+    num: number;
+    name: string;
+    chromas: boolean;
+}[]
+export type info = {
+    attack: number;
+    defense: number;
+    magic: number;
+    difficulty: number;
+};
+export type stats = {
+    hp: number;
+    hpperlevel: number;
+    mp: number;
+    mpperlevel: number;
+    movespeed: number;
+    armor: number;
+    armorperlevel: number;
+    spellblock: number;
+    spellblockperlevel: number;
+    attackrange: number;
+    hpregen: number;
+    hpregenperlevel: number;
+    mpregen: number;
+    mpregenperlevel: number;
+    crit: number;
+    critperlevel: number;
+    attackdamage: number;
+    attackdamageperlevel: number;
+    attackspeedperlevel: number;
+    attackspeed: number;
+};
+export type spells = {
+    id: string;
+    name: string;
+    description: string;
+    tooltip: string;
+    leveltip: {
+        label: string[];
+        effect: string[];
+    };
+    maxrank: number;
+    cooldown: number[];
+    cooldownBurn: string;
+    cost: number[];
+    costBurn: string;
+    resource: string;
+    range: number[];
+    rangeBurn: string;
+}[];
+export type passive = {
+    name: string;
+    description: string;
+    image: {
+        full: string;
+        sprite: string;
+        group: string;
+        x: number;
+        y: number;
+        w: number;
+        h: number;
+    };
+}
 export type championDataType<T> = {
     type: string;
     format: string;
@@ -8,86 +82,18 @@ export type championDataType<T> = {
             key: string;
             name: string;
             title: string;
-            image: {
-                full: string;
-                sprite: string;
-                group: string;
-                x: number;
-                y: number;
-                w: number;
-                h: number;
-            };
-            skins: {
-                id: string;
-                num: number;
-                name: string;
-                chromas: boolean;
-            }[];
+            image: image
+            skins: skins
             lore: string;
             blurb: string;
             allytips: string[];
             enemytips: string[];
             tags: string[];
             partype: string;
-            info: {
-                attack: number;
-                defense: number;
-                magic: number;
-                difficulty: number;
-            };
-            stats: {
-                hp: number;
-                hpperlevel: number;
-                mp: number;
-                mpperlevel: number;
-                movespeed: number;
-                armor: number;
-                armorperlevel: number;
-                spellblock: number;
-                spellblockperlevel: number;
-                attackrange: number;
-                hpregen: number;
-                hpregenperlevel: number;
-                mpregen: number;
-                mpregenperlevel: number;
-                crit: number;
-                critperlevel: number;
-                attackdamage: number;
-                attackdamageperlevel: number;
-                attackspeedperlevel: number;
-                attackspeed: number;
-            };
-            spells: {
-                id: string;
-                name: string;
-                description: string;
-                tooltip: string;
-                leveltip: {
-                    label: string[];
-                    effect: string[];
-                };
-                maxrank: number;
-                cooldown: number[];
-                cooldownBurn: string;
-                cost: number[];
-                costBurn: string;
-                resource: string;
-                range: number[];
-                rangeBurn: string;
-            }[];
-            passive: {
-                name: string;
-                description: string;
-                image: {
-                    full: string;
-                    sprite: string;
-                    group: string;
-                    x: number;
-                    y: number;
-                    w: number;
-                    h: number;
-                };
-            };
+            info: info;
+            stats: stats;
+            spells: spells;
+            passive: passive;
             recommended: T[];
         };
     };
