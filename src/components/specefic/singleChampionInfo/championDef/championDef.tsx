50affc7ada14d4bf9d662b0d250dcc1653d7a8a3
alaasassii <alaasassijs@gmail.com>
2023-09-21 18:22:15 +0100

    add new things

--- src/components/specefic/singleChampionInfo/championDef/championDef.tsx
+++ src/components/specefic/singleChampionInfo/championDef/championDef.tsx
@@ -1,6 +1,47 @@
 import React from 'react'
 import CardContainer from '../../../common/cardContainer'
 type ChampionDef = {
+    image: {
+        full: string;
+        sprite: string;
+        group: string;
+        x: number;
+        y: number;
+        w: number;
+        h: number;
+    },
+    title: string,
+    spells: {
+        id: string;
+        name: string;
+        description: string;
+        tooltip: string;
+        leveltip: {
+            label: string[];
+            effect: string[];
+        };
+        maxrank: number;
+        cooldown: number[];
+        cooldownBurn: string;
+        cost: number[];
+        costBurn: string;
+        resource: string;
+        range: number[];
+        rangeBurn: string;
+    }[],
+    passive: {
+        name: string;
+        description: string;
+        image: {
+            full: string;
+            sprite: string;
+            group: string;
+            x: number;
+            y: number;
+            w: number;
+            h: number;
+        };
+    }
 
 }
 const ChampionDef = () => {
