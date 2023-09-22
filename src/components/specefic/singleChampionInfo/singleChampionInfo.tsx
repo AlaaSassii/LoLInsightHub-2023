061763c6ed8322a6de98e350ac2abda0b1418707
alaasassii <alaasassijs@gmail.com>
2023-09-22 14:12:43 +0100

    add new things

--- src/components/specefic/singleChampionInfo/singleChampionInfo.tsx
+++ src/components/specefic/singleChampionInfo/singleChampionInfo.tsx
@@ -2,7 +2,7 @@ import { useParams } from "react-router-dom"
 import Container from "../../common/container"
 import { useAppDispatch } from "../../../hooks/useAppDispatch";
 import { useAppSelector } from "../../../hooks/useAppSelectore";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { fetchChampionData } from "../../../redux/SingleChampionSlice";
 import MainLoadingSpinner from "../../common/mainLoadingSpinner";
 import ChampionDef from "./championDef";
@@ -13,6 +13,7 @@ import ChampionInfo from "./championInfo";
 import ChampionSkins from "./championSkins";
 const SingleChampionInfo = () => {
     const { name } = useParams();
+    console.log({ name })
     const dispatch = useAppDispatch();
     const { data: champion, loading, error } = useAppSelector(state => state.singleChampion);
     useEffect(() => {
@@ -21,7 +22,7 @@ const SingleChampionInfo = () => {
             region: 'en_US',
             name: name || ''
         }
-        dispatch(fetchChampionData(argument))
+        dispatch(fetchChampionData(argument));
     }, [])
     if (error) return error;
     if (loading) return <MainLoadingSpinner />
@@ -31,18 +32,18 @@ const SingleChampionInfo = () => {
         <Container>
             <ChampionDef
                 name={name as string}
-                title={champion?.data?.[name as string].title}
+                title={champion?.data?.[name as string]?.title}
                 lore={champion?.data?.[name as string]?.lore}
-                spells={champion?.data?.[name as string].spells}
-                passive={champion?.data?.[name as string].passive} />
+                spells={champion?.data?.[name as string]?.spells}
+                passive={champion?.data?.[name as string]?.passive} />
             <ChampionType
-                tags={champion?.data?.[name as string].tags}
+                tags={champion?.data?.[name as string]?.tags}
             />
             <AllyTips
-                allytips={champion?.data?.[name as string].allytips}
+                allytips={champion?.data?.[name as string]?.allytips}
             />
             <EnemyTips
-                enemytips={champion?.data?.[name as string].enemytips}
+                enemytips={champion?.data?.[name as string]?.enemytips}
             />
             <ChampionInfo info={champion?.data?.[name as string]?.info} />
             <ChampionSkins name={name} skins={champion?.data?.[name as string]?.skins} />
