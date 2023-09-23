ea89e0cde968822318aef1eea31fd52276ee66dd
alaasassii <alaasassijs@gmail.com>
2023-09-23 18:54:30 +0100

    add new things

---
+++ src/types/rankType.ts
@@ -0,0 +1,17 @@
+type LeagueInfo = {
+    leagueId: string;
+    queueType: string;
+    tier: string;
+    rank: string;
+    summonerId: string;
+    summonerName: string;
+    leaguePoints: number;
+    wins: number;
+    losses: number;
+    veteran: boolean;
+    inactive: boolean;
+    freshBlood: boolean;
+    hotStreak: boolean;
+};
+
+export type rankType = [] | LeagueInfo[]; 
