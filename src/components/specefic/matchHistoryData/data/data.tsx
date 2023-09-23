7d79cf378de3440ce5374eafcb2a20dfb9209dd9
alaasassii <alaasassijs@gmail.com>
2023-09-23 19:25:01 +0100

    update the logic

--- src/components/specefic/matchHistoryData/data/data.tsx
+++ src/components/specefic/matchHistoryData/data/data.tsx
@@ -3,17 +3,21 @@ import { summonerInfoType } from '../../../../types/summonerInfoType'
 import Container from '../../../common/container'
 import UserTitle from './userTitle'
 import UserChampions from './userChampions'
+import UserRankInfo from './userRankInfo'
 
 type dataProps = summonerInfoType & {
     error: string
 }
-const Data: FC<dataProps> = ({ id, accountId, name, profileIconId, puuid, revisionDate, summonerLevel, error }) => {
+const Data: FC<dataProps> = ({ id: summonerId, accountId, name, profileIconId, puuid, revisionDate, summonerLevel, error }) => {
     return (
         <Container className='user__data__container'>
             <UserTitle
                 name={name}
                 profileIconId={profileIconId}
                 error={error}
+            />
+            <UserRankInfo
+                summonerId={summonerId}
             />
             <UserChampions
                 puuid={puuid}
--- src/components/specefic/matchHistoryData/data/userRankInfo/userRankInfo.tsx
+++ src/components/specefic/matchHistoryData/data/userRankInfo/userRankInfo.tsx
@@ -1,9 +1,22 @@
-import { FC } from 'react'
+import { FC, useEffect } from 'react'
 import CardContainer from '../../../../common/cardContainer'
+import axios, { AxiosError, AxiosResponse } from 'axios'
+import { Api__key } from '../../../../../services/apiKey'
 type userRankInfoProps = {
-
+    summonerId: string
+    error: string
 }
-const UserRankInfo: FC<userRankInfoProps> = ({ }) => {
+const UserRankInfo: FC<userRankInfoProps> = ({ summonerId, error }) => {
+
+    useEffect(() => {
+        axios.get(`https://euw1.api.riotgames.com/lol/league/v4/entries/by-summoner/${summonerId}?api_key=${Api__key}`)
+            .then((response: AxiosResponse) => {
+
+            })
+            .catch((error: AxiosError) => {
+
+            })
+    }, [])
     return (
         <CardContainer className='' loading={false}>
 
