556f847f0a8b3a6ff011de8756f9b58be1f47ba7
alaasassii <alaasassijs@gmail.com>
2023-10-16 10:37:44 +0100

    add new things

--- src/services/apiKey.ts
+++ src/services/apiKey.ts
@@ -1,1 +1,2 @@
-export const Api__key: string = 'RGAPI-3b029f79-c08f-4785-88c0-b6f92549044c'
+export const Api__key: string = 'RGAPI-3c298e23-8274-4eb6-8c2b-86b42f644783'
+// RGAPI-3c298e23-8274-4eb6-8c2b-86b42f644783
