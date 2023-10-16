e0134c9003935b77038b8c8275b6cccbd9a77e0f
alaasassii <alaasassijs@gmail.com>
2023-10-16 20:05:30 +0100

    add new things

--- src/routes/matchHistoryData/matchHistoryData.tsx
+++ src/routes/matchHistoryData/matchHistoryData.tsx
@@ -1,10 +1,8 @@
-// import PageContainer from '../../components/common/pageContainer'
 import PageContainer from '../../components/common/pageContainer'
 import PageUnavailable from '../../components/common/pageUnavailable'
 const MatchHistoryData = () => {
     return (
         <PageContainer  >
-            {/* <MatchHistory /> */}
             <PageUnavailable />
         </PageContainer>
     )
