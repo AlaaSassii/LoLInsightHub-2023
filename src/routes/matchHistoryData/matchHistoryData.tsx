cbc1f4e75afbbaae1b0ad8739688dd54971cb48b
alaasassii <alaasassijs@gmail.com>
2023-10-21 18:04:24 +0100

    add responsiveness in champion info

--- src/routes/matchHistoryData/matchHistoryData.tsx
+++ src/routes/matchHistoryData/matchHistoryData.tsx
@@ -3,7 +3,8 @@ import PageUnavailable from '../../components/common/pageUnavailable'
 const MatchHistoryData = () => {
     return (
         <PageContainer  >
-            <PageUnavailable />
+            <PageUnavailable
+                paragraph='We eagerly await Riot Games to provide a permanent API key, simplifying the process of making heavy requests for accessing comprehensive match history data and additional information. This key will streamline the retrieval of essential gaming statistics, making it more accessible and user-friendly for all enthusiasts and developers alike.' />
         </PageContainer>
     )
 }
