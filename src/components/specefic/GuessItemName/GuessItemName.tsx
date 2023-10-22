ba91ad350f6ccba4bfc3b22802d0bb00577ee887
alaasassii <alaasassijs@gmail.com>
2023-10-22 11:32:27 +0100

    add mode game enum

---
+++ src/components/specefic/GuessItemName/GuessItemName.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const GuessItemName = () => {
+    return (
+        <div className='guess__item__name'>
+
+        </div>
+    )
+}
+
+export default GuessItemName
---
+++ src/enums/modeGame.ts
@@ -0,0 +1,5 @@
+export enum modeGame {
+    easy = "EASY",
+    medium = "MEDIUM",
+    hard = "HARD",
+}
