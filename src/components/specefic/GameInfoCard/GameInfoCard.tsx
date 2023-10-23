8436395cb902ff8faee25587f9b4d10149b7bda9
alaasassii <alaasassijs@gmail.com>
2023-10-23 09:05:41 +0100

    add new things

--- src/components/specefic/GameInfoCard/GameInfoCard.tsx
+++ src/components/specefic/GameInfoCard/GameInfoCard.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from 'react'
+import { FC } from 'react'
 import './GameInfoCard.scss'
 import { links } from '../../../enums/routes/links'
 import { useNavigate } from 'react-router-dom'
--- src/consts/gamesCard.ts
+++ src/consts/gamesCard.ts
@@ -1,28 +1,32 @@
 import { links } from "../enums/routes/links";
-
+import q from '../assets/images/wallpaper/qiyana-4096x2303-16939.jpg';
+import akaliJinx from '../assets/images/wallpaper/akalijinx.jpg';
+import akaliysas from '../assets/images/wallpaper/akaliyass.jpg';
+import mf from '../assets/images/wallpaper/mf.jpg'
+import w4 from '../assets/images/wallpaper/w4.webp'
 export const gamesCars = [
     {
         title: 'card game',
         description: 'Test your memory with hidden champions! Uncover pairs, master abilities, and become the ultimate Summoner in this fast-paced memory game set in the world of League of Legends.',
-        image: 'https://www.wallpapers13.com/wp-content/uploads/2015/11/League-Of-Legends-Champion-Annie-the-Dark-Child-and-Riven-the-exile-4k-ultra-HD-wallpapers-for-high-resolution-computer-and-laptop-1680x1050.jpeg',
+        image: akaliJinx,
         link: links.CARDCHAMPIONSGAME,
     },
     {
         title: 'Guess Item',
         description: 'Hone your item-naming expertise with this engaging game. Test your ability to identify a wide range of items and become a name-finding pro!',
-        image: 'https://wallpapercg.com/download/qiyana-4096x2303-16939.jpeg',
+        image: akaliysas,
         link: links.GUESSITEM,
     },
     {
         title: 'Quiz Game',
         description: 'Test your gaming knowledge with questions and challenges. Are you a true gaming expert? Show what you know!',
-        image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQUJY8fiDk5IxhdRiHu8W7Jpbl_m7icQQpOzEFkGOe7BNiP227DdRtyzddVm5f3kQcCS_Q&usqp=CAU',
+        image: mf,
         link: links.QUIZGAME,
     },
     {
         title: 'Guess Champion',
         description: 'Challenge your League of Legends knowledge by guessing champion names. Can you name them all and become a champion expert? Test your skills now!',
-        image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSGkueAn9lTTwcHs_FPdvVdy1LTnPdjMsG7cg&usqp=CAU',
+        image: w4,
         link: links.GUESSCHAMPION,
     },
 ]
